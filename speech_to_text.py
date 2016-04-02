1dda7974041c0afcaa0841591fbc9e38c825ef0b
Elizabeth Hau class of 2016 <hhau@tempest.wellesley.edu>
2016-04-02 15:12:48 -0400

    decoder not working

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,7 +1,8 @@
-import audiotools as at
-import pocketsphinx as ps
+#import audiotools as at
+from pocketsphinx.pocketsphinx import *
+from os import environ, path
 
-#MODELDIR = "pocketsphinx-python/pocketsphinx/model"
+MODELDIR = "/home/sravana/applications/pocketsphinx-python/pocketsphinx/model"
 
 #E.g:
 #http://www.repository.voxforge1.org/downloads/Main/Trunk/AcousticModels/Sphinx/voxforge-en-r0_1_3.tar.gz
@@ -14,10 +15,55 @@ lmd = '/home/sravana/applications/pocketsphinx-python/pocketsphinx/model/en-us/e
 #dictd = '/<your_path>/models/cmudict.hub4.06d.dict'
 dictd = '/home/sravana/applications/pocketsphinx-python/pocketsphinx/model/en-us/cmudict-en-us.dict'
 
-speechRec = ps.Decoder(hmm = hmmd, lm = lmd, dict = dictd)
+# create a decoder
+config = Decoder.default_config()
+config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
+config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
+config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
+
+decoder = Decoder(config)
 #make sure that your audio file has a wav format, sample format 16 bit PCM mono 
 wavFile = open('test-hello.wav', 'rb')
-wavFile.seek(44)
+#wavFile.seek(44)
+'''
 speechRec.decode_raw(wavFile)
 result = speechRec.get_hyp()
+print type(result)
+print 'result is', result
+
 print result[0]
+'''
+
+decoder.start_utt()
+stream = wavFile
+while True:
+    buf = stream.read(1024)
+    print buf
+    if buf:
+        print '\nbuf was true'
+        decoder.process_raw(buf, False, False)
+    else:
+        break
+    decoder.end_utt()
+    hypothesis = decoder.hyp()
+    print type(hypothesis)
+    print hypothesis
+    logmath = decoder.get_logmath()
+    print ('Best hypothesis: ', hypothesis.hypstr, " model score: ", hypothesis.best_score, " confidence: ", logmath.exp(hypothesis.prob))
+
+    print ('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
+'''
+# Access N best decodings.
+print ('Best 10 hypothesis: ')
+for best, i in zip(decoder.nbest(), range(10)):
+        print (best.hypstr, best.score)
+
+        stream = open(path.join(DATADIR, 'goforward.mfc'), 'rb')
+        stream.read(4)
+        buf = stream.read(13780)
+        decoder.start_utt()
+        decoder.process_cep(buf, False, True)
+        decoder.end_utt()
+        hypothesis = decoder.hyp()
+        print ('Best hypothesis: ', hypothesis.hypstr, " model score: ", hypothesis.best_score, " confidence: ", hypothesis.prob)
+'''
