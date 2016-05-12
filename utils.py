d5b55aa000ddc2059de43c8e3c76285765e2e6fc
Elizabeth Hau <elizabeth.h.hau@gmail.com>
2016-05-12 10:56:28 -0400

    reset silence count & read keyboard input

--- utils.py
+++ utils.py
@@ -1,16 +1,32 @@
 from sys import byteorder
 from array import array
 from struct import pack
-
+import select
+import sys
 import pyaudio
 import wave
-''' source: http://stackoverflow.com/questions/892199/detect-record-audio-in-python '''
 
-THRESHOLD = 300
+''' sources: 
+    1. http://stackoverflow.com/questions/892199/detect-record-audio-in-python
+    2. http://stackoverflow.com/questions/292095/polling-the-keyboard-detect-a-keypress-in-python
+'''
+
+THRESHOLD = 300 # threshold for when to "tag" something as silent
+SILENT_THRESHOLD = 30 # threshold for when to stop recording
 CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
-RATE = 16000
+RATE = 16000 # sampling rate
 
+def heardEnter():
+    "Returns 'True' if the 'Enter' key is pressed"
+    i,o,e = select.select([sys.stdin],[],[],0.0001)
+    
+    for s in i:
+        if s == sys.stdin:
+            input = sys.stdin.read(1)
+            return True
+    return False
+    
 def is_silent(snd_data):
     "Returns 'True' if below the 'silent' threshold"
     return max(snd_data) < THRESHOLD
@@ -61,6 +77,10 @@ def record():
     Record a word or words from the microphone and 
     return the data as an array of signed shorts.
 
+    Stops recording when the silence count is over
+    the SILENCE_THRESHOLD or when the 'Enter' key 
+    is pressed
+    
     Normalizes the audio, trims silence from the 
     start and end, and pads with 0.5 seconds of 
     blank sound to make sure VLC et al can play 
@@ -73,25 +93,40 @@ def record():
 
     num_silent = 0
     snd_started = False
-
+    was_silent = False
+    
     r = array('h')
 
     while 1:
+        
         # little endian, signed short
         snd_data = array('h', stream.read(CHUNK_SIZE))
         if byteorder == 'big':
             snd_data.byteswap()
         r.extend(snd_data)
-
+        
         silent = is_silent(snd_data)
 
         if silent and snd_started:
             num_silent += 1
+            was_silent = True
+            
         elif not silent and not snd_started:
             snd_started = True
-
-        if snd_started and num_silent > 30:
+            
+        # reset num_silent if was silent but now talking again
+        elif not silent and was_silent:
+            num_silent = 0
+            was_silent = False
+        
+        # break if num_silence is over the threshold
+        if snd_started and num_silent > SILENT_THRESHOLD:
             break
+            
+        # or break if enter key is pressed
+        if heardEnter():
+            break
+            
 
     sample_width = p.get_sample_size(FORMAT)
     stream.stop_stream()
@@ -116,6 +151,6 @@ def record_to_file(path):
     wf.close()
 
 if __name__ == '__main__':
-    print("please speak a word into the microphone")
+    print("*** please speak into the microphone ***\n")
     record_to_file('demo.wav')
-    print("done - result written to demo.wav")
+    print("\n*** done - result written to demo.wav ***")
